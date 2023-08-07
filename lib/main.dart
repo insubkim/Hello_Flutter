2cdc1c524408c77e33f9689e2c43a9bfb774ef44
MoongMoongBook <j.j.kant1948@gmail.com>
2023-08-07 23:17:11 +0900

    feat : made stateful widget, learn initstate, build, dispose, widget tree, widget lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,82 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool show = true;
+
+  void toggle() {
+    setState(() {
+      show = !show;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: const TextTheme(
+              titleLarge: TextStyle(
+        color: Colors.red,
+      ))),
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'count',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.black,
+                ),
+              ),
+              show ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                iconSize: 70,
+                color: Colors.purple,
+                onPressed: toggle,
+                icon: const Icon(Icons.add_circle),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-    throw UnimplementedError();
   }
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    print("hi");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("hey");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print("bye");
+    return Text(
+      'large title',
+      style: TextStyle(
+        fontSize: 80,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
+    );
   }
 }
