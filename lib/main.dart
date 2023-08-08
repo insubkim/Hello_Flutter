7118f3075c302dcbfe0c5a12005a4cf6cbc8b379
MoongMoongBook <j.j.kant1948@gmail.com>
2023-08-08 23:27:25 +0900

    feat : api date fetching

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(backgroundColor: Colors.red),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: Scaffold(
-        body: Center(
-          child: HomeScreen(),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,148 +10,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const int twentyfive = 3;
-  int pomorodo = 0;
-  int totalSeconds = twentyfive;
-  bool isRunning = false;
-  late Timer timer;
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
 
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    print(duration.toString().split('.').first.substring(2));
-    return duration.toString().split('.').first.substring(2);
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
   }
 
-  void reset() {
-    setState(() {
-      totalSeconds = twentyfive;
-      isRunning = false;
-    });
-    timer.cancel();
-  }
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        pomorodo++;
-        totalSeconds = twentyfive;
-        isRunning = false;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds--;
-      });
-    }
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(const Duration(seconds: 1), onTick);
-    isRunning = true;
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                decoration: const BoxDecoration(),
-                child: Text(
-                  format(totalSeconds),
-                  style: TextStyle(
-                    color: Theme.of(context).cardColor,
-                    fontSize: 80,
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Center(
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(),
-                      child: IconButton(
-                        iconSize: 120,
-                        color: Theme.of(context).cardColor,
-                        onPressed: isRunning ? onPausePressed : onStartPressed,
-                        icon: Icon(
-                          isRunning
-                              ? Icons.pause_circle_filled_outlined
-                              : Icons.play_circle_fill_outlined,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      decoration: const BoxDecoration(),
-                      child: IconButton(
-                        iconSize: 80,
-                        color: Theme.of(context).cardColor,
-                        onPressed: reset,
-                        icon: const Icon(
-                          Icons.restart_alt,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Pomodoros',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge!
-                                  .color,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Text(
-                            '$pomorodo',
-                            style: TextStyle(
-                              fontSize: 60,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge!
-                                  .color,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          'today\'s 툰s ',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
   }
 }
